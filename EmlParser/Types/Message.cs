6b736b31adcdfb215c7fc3a6639c2087534015a9
geffzhang <geffzhang@qq.com>
2023-06-04 12:21:29 +0800

    feat: ReadSharp Port

--- EmlParser/Types/Message.cs
+++ EmlParser/Types/Message.cs
@@ -13,16 +13,16 @@ internal record Message
     public override string ToString()
     {
         var sb = new StringBuilder();
-        sb.AppendLine($"Тема: {Subject}");
-        sb.AppendLine( $"От:");
+        sb.AppendLine($"主题: {Subject}");
+        sb.AppendLine( $"发送人:");
         sb.AppendLine($"\t{From.Name} {From.Email}");
-        sb.AppendLine("Для:");
+        sb.AppendLine("接收人:");
         sb.AppendLine(string.Join("", To.Select(u=>$"\n\t{u.Name} {u.Email}")));
-        sb.AppendLine("Письмо:");
+        sb.AppendLine("内容:");
         sb.AppendLine(string.Join("\n\t", HtmlUtilities.ConvertToPlainText(Content).Split("\n")));
         if (Attachments.Any())
         {
-            sb.AppendLine("Вложения: ");
+            sb.AppendLine("附件: ");
             sb.AppendLine(string.Join("", Attachments.Select(u => $"\n\t{u.FileName}")));
         }
         return sb.ToString();
